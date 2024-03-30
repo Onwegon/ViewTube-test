5246442e01f5c178a69888ebda666e0933bbbd7b
FAD <fdkjri@gmail.com>
2024-03-30 13:35:43 +0300

    Add project

---
+++ appTest/appTest/appTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  appTestApp.swift
+//  appTest
+//
+//  Created by Пользователь on 30.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct appTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
